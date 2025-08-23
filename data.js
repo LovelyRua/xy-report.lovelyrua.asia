4dcf2545b72479ba570fd054a40ce821f2799e40
LovelyRua <81335837+LovelyRua@users.noreply.github.com>
2025-08-23 12:05:23 +0800

    Update data.js

--- data.js
+++ data.js
@@ -209,7 +209,7 @@ const transactionData = [
 // 分类统计
 const categoryStats = {
     income: {
-        '赞助打赏': 470,
+        '赞助打赏': 569,
         '算力出租': 59
     },
     expense: {
@@ -232,13 +232,14 @@ const monthlyTrends = [
     { month: '2025-03', income: 160, expense: -399 },
     { month: '2025-04', income: 0, expense: -18 },
     { month: '2025-06', income: 0, expense: -180 },
-    { month: '2025-08', income: 24, expense: -475.8 }
+    { month: '2025-08', income: 123, expense: -475.8 }
 ];
 
 // 导出数据
 window.transactionData = transactionData;
 window.categoryStats = categoryStats;
 window.monthlyTrends = monthlyTrends;
+
 
 
 
