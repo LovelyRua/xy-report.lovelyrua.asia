aa80369a38f48f33891591ebcd61223587ea3a65
LovelyRua <cutelaura@foxmail.com>
2026-04-01 20:28:53 +0800

    update data

--- data.js
+++ data.js
@@ -217,6 +217,14 @@ const transactionData = [
         amount: 36,
         details: '吾 暖心打赏36元'
     },
+	{
+        date: '2026-03-31',
+        type: 'income',
+        category: '赞助打赏',
+        description: 'GunterW',
+        amount: 100,
+        details: 'GunterW 为爱发电赞助100元'
+    },
     
     // 支出记录
     {
@@ -424,7 +432,7 @@ const transactionData = [
 // 分类统计
 const categoryStats = {
     income: {
-        '赞助打赏': 1492.8,
+        '赞助打赏': 1592.8,
         '算力出租': 59
     },
     expense: {
@@ -453,7 +461,7 @@ const monthlyTrends = [
     { month: '2025-12', income: 36, expense: -180 },
     { month: '2026-01', income: 200, expense: 0 },
     { month: '2026-02', income: 352, expense: -30 },
-    { month: '2026-03', income: 81, expense: 266.08 }
+    { month: '2026-03', income: 181, expense: 266.08 }
 ];
 
 // 导出数据
